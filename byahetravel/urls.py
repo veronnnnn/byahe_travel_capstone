563b5a1489bba415d6c0d709875aa1914097736a
clev225 <clevenjavier2021@gmail.com>
2024-09-29 10:14:31 +0800

    url changes

--- byahetravel/urls.py
+++ byahetravel/urls.py
@@ -20,10 +20,16 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.index, name='index'),
+
+    path('user/signin/', views.sign_in, name='sign_in'),
+    path('user/signup/', views.sign_up, name='sign_up'),
+    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- byahetravel/views.py
+++ byahetravel/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def sign_in(request):
+    return render(request, 'sign-in.html')
+
+def sign_up(request):
+    return render(request, 'sign-up.html')
